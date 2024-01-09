64a57317a49a2ba0f09685479aa162c2b1801933
unknown <shyamgupta1.nmims@gmail.com>
2024-01-09 23:08:59 +0530

    find the largest element present in an array

---
+++ src/Array_Placement/findMaxElement.java
@@ -0,0 +1,44 @@
+package Array_Placement;
+
+public class findMaxElement {
+	
+	public static int findLargest(int[] arr) {
+		int max = Integer.MIN_VALUE;
+		for(int i =0 ; i<arr.length; i++) {
+			if(arr[i] > max) {
+				max = arr[i];
+			}
+		}
+		return max;
+		
+		
+		
+		
+//		public static int linearSearchAlgo(int[] arr, int elem) {
+//			for(int i = 0; i< arr.length; i++) {
+//				if(arr[i] == elem) {
+//					return i;
+//				}
+				
+	}	
+	
+	public static int findIndesOfMax(int[] arr, int max) {
+		for(int i =0; i<arr.length; i++) {
+			if(arr[i] == max) {
+				return i;
+			}
+		}
+		return -1;
+	}
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] arr = {3,11,1,4,90};
+		int Max = findLargest(arr);
+		System.out.println(Max);
+		int index = findIndesOfMax(arr, Max);
+		System.out.println("Max is present at index no-" + " "+ index);
+	}
+
+}
