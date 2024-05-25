46050f2c60841bb7414665fa8374e3bbc6d583e1
Shyam Gupta <shyamgupta1.nmims@gmail.com>
2024-05-25 18:09:45 +0530

    Real InterView Question

---
+++ src/Selenium_javaInterviewRealQuestion/PrintFibonacciSeries.java
@@ -0,0 +1,23 @@
+package Selenium_javaInterviewRealQuestion;
+
+public class PrintFibonacciSeries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n =10;
+		int firstTerm = 0;
+		int secondTerm = 1;
+		int nextTerm;
+		System.out.println("Fibbonaci series till " + n + " terms:");
+		
+		for(int i =0; i<n; i++) {
+			System.out.print(firstTerm+ ",");
+			nextTerm = firstTerm + secondTerm;
+			firstTerm = secondTerm;
+			secondTerm = nextTerm;
+		}
+
+	}
+
+}
