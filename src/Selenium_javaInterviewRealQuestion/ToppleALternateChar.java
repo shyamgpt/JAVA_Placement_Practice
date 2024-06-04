76f2993a3e2278bfe519c1a631d2373b4fa53cdb
Shyam Gupta <shyamgupta1.nmims@gmail.com>
2024-06-04 08:58:30 +0530

    Chat from upperCase to LowerCase

--- src/Selenium_javaInterviewRealQuestion/ToppleALternateChar.java
+++ src/Selenium_javaInterviewRealQuestion/ToppleALternateChar.java
@@ -1,17 +1,16 @@
 package Selenium_javaInterviewRealQuestion;
 
-//Ques -->convert mAdAm --> MadaM
-
-public class StringUppToLowerCase {
+public class ToppleALternateChar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
 		String str  = "heelo every one";
 		String  x = "";
 		for(int i =0; i<str.length(); i++) {
 			char ch = str.charAt(i);
-			if(Character.isUpperCase(ch)) {
+			if(i%2!=0) {
 				x = x+ Character.toLowerCase(ch);
 			}
 			else {
@@ -19,6 +18,8 @@ public class StringUppToLowerCase {
 			}
 		}
 		System.out.println(x);
+		//hEeLo eVeRy oNe
+
 	}
 
 }
